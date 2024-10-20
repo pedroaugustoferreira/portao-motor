8ad2abc1f055c15f0c0fbfb6ca5f0d7fdc978cec
pedroaugustoferreira <43154432+pedroaugustoferreira@users.noreply.github.com>
2024-10-20 01:59:06 -0300

    Update arduino-mega-ok.ino

--- arduino-mega-ok.ino
+++ arduino-mega-ok.ino
@@ -15,6 +15,7 @@ const int sensor_aberto = 5; // GPIO5 = D5
 const int ABRINDO = 0;
 const int PARADO = 1;
 const int FECHANDO = 2;
+int tempo_delay = 500;
 
 const int pino_ir = 0; // mega pino 2
 const int rele_aberto = 22;
@@ -22,6 +23,9 @@ const int rele_fechado = 26;
 
 int proximo_estado = ABRINDO;
 int ultima_acao;
+
+// Array com os códigos válidos
+long codigos_validos[] = {8632593, 8632594};
 
 void setup() {
   // Configuração inicial
@@ -42,72 +46,87 @@ void setup() {
 void loop() {
   // Verifica se há dados disponíveis do receptor
   if (mySwitch.available()) {
-    // Alterna entre os estados
-    if (proximo_estado == ABRINDO) {
-      
-      Serial.println("Portão Abrindo");
-      digitalWrite(rele_aberto, LOW);
-      digitalWrite(rele_fechado, HIGH);
-      proximo_estado = PARADO;
-      delay(2000);
-    } 
-    else if (proximo_estado == FECHANDO) {
-      Serial.println("Portão Fechando");
-      digitalWrite(rele_aberto, HIGH);
-      digitalWrite(rele_fechado, LOW);
-      proximo_estado = PARADO;
-    } 
-    else if (proximo_estado == PARADO) {
-      Serial.println("Portão Parado");
-      digitalWrite(rele_aberto, HIGH);
-      digitalWrite(rele_fechado, HIGH);
-
-      // Alterna o próximo estado entre ABRINDO e FECHANDO
-      if (ultima_acao == ABRINDO) {
-        proximo_estado = FECHANDO;
-        ultima_acao = FECHANDO;
-      } else {
-        proximo_estado = ABRINDO;
-        ultima_acao = ABRINDO;
+    // Obtém o código recebido
+    long codigo_recebido = mySwitch.getReceivedValue();
+    
+    // Verifica se o código recebido está no array de códigos válidos
+    bool codigo_valido = false;
+    for (int i = 0; i < sizeof(codigos_validos) / sizeof(codigos_validos[0]); i++) {
+      if (codigo_recebido == codigos_validos[i]) {
+        codigo_valido = true;
+        break;
       }
     }
+    
+    // Executa a ação apenas se o código for válido
+    if (codigo_valido) {
+      // Alterna entre os estados
+      if (proximo_estado == ABRINDO) {
+        Serial.print("Portão Abrindo - delay: ");
+        Serial.println(tempo_delay);
+        digitalWrite(rele_aberto, LOW);
+        digitalWrite(rele_fechado, HIGH);
+        proximo_estado = PARADO;
+        delay(tempo_delay);
+        // tempo_delay = 500;
+      } 
+      else if (proximo_estado == FECHANDO) {
+        Serial.print("Portão Fechando - delay: ");
+        Serial.println(tempo_delay);
+        digitalWrite(rele_aberto, HIGH);
+        digitalWrite(rele_fechado, LOW);
+        proximo_estado = PARADO;
+        delay(tempo_delay);
+        // tempo_delay = 500;
+      } 
+      else if (proximo_estado == PARADO) {
+        Serial.print("Portão Parado - delay: ");
+        Serial.println(tempo_delay);
+        digitalWrite(rele_aberto, HIGH);
+        digitalWrite(rele_fechado, HIGH);
 
+        // Alterna o próximo estado entre ABRINDO e FECHANDO
+        if (ultima_acao == ABRINDO) {
+          proximo_estado = FECHANDO;
+          ultima_acao = FECHANDO;
+        } else {
+          proximo_estado = ABRINDO;
+          ultima_acao = ABRINDO;
+        }
+      }
 
-    Serial.print("Recebido: ");
-    Serial.print(mySwitch.getReceivedValue());      
-    Serial.print(" / ");
-    Serial.print(mySwitch.getReceivedBitlength());
-    Serial.print("bit ");
-    Serial.print("Protocolo: ");
-    Serial.println(mySwitch.getReceivedProtocol());
+      Serial.print("Código válido recebido: ");
+      Serial.println(codigo_recebido);
+    } else {
+      Serial.print("Código inválido: ");
+      Serial.println(codigo_recebido);
+    }
 
-    delay(1000); // Atraso para simular a ação
+    delay(tempo_delay); // Atraso para simular a ação
     mySwitch.resetAvailable(); // Reseta o status de disponibilidade do receptor
-
-    // (Comentado) Código para depuração - Recebimento de dados do RCSwitch
-    // Serial.print("Recebido: ");
-    // Serial.print(mySwitch.getReceivedValue());      
-    // Serial.print(" / ");
-    // Serial.print(mySwitch.getReceivedBitlength());
-    // Serial.print("bit ");
-    // Serial.print("Protocolo: ");
-    // Serial.println(mySwitch.getReceivedProtocol());
   }
 
   // Verifica se o botão foi pressionado
   if (digitalRead(sensor_fechado) == LOW) {
-    Serial.println("Portão fechado por completo");
+    Serial.print("Portão fechado por completo - delay: ");
+    Serial.println(tempo_delay);
     digitalWrite(rele_aberto, HIGH);
     digitalWrite(rele_fechado, HIGH);
     proximo_estado = ABRINDO;
-    delay(1000); // Atraso para evitar múltiplas leituras do botão
+    // delay(tempo_delay);
+    tempo_delay = 3000; // Atraso para evitar múltiplas leituras do botão
   }
-    if (digitalRead(sensor_aberto) == LOW) {
-    Serial.println("Portão aberto por completo");
+  else if (digitalRead(sensor_aberto) == LOW) {
+    Serial.print("Portão aberto por completo - delay: ");
+    Serial.println(tempo_delay);
     digitalWrite(rele_aberto, HIGH);
     digitalWrite(rele_fechado, HIGH);
     proximo_estado = FECHANDO;
-    delay(1000); // Atraso para evitar múltiplas leituras do botão
+    // delay(tempo_delay);
+    tempo_delay = 3000; // Atraso para evitar múltiplas leituras do botão
+  }else
+  {
+    tempo_delay = 500;
   }
 
 }
